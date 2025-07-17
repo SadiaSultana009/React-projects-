5b67de164688592bae06bce2c368eca751fe8ee4
SadiaSultana009 <09sadia.sultana@gmail.com>
2025-07-17 19:54:03 +0600

    build tab component

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import menus from "./Components/tree-view/data";
 import QRCodeGenerator from "./Components/qr-code-generator";
 import LightDarkMode from "./Components/light-dark-mode";
 import ScrollIndicator from "./Components/scroll-indicator";
+import TabTest from "./Components/custom-tab/tab-test";
 
 function App() {
     return (
@@ -41,7 +42,10 @@ function App() {
             {/* <LightDarkMode /> */}
 
             {/* Scroll indicator component */}
-            <ScrollIndicator url={"https://dummyjson.com/products?limit=100"} />
+            {/* <ScrollIndicator url={"https://dummyjson.com/products?limit=100"} /> */}
+
+            {/* Custom tabs component */}
+            <TabTest />
         </div>
     );
 }
